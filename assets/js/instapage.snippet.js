b6159e8884bb1720b46755c48c36c63ca93e760a
Peter Yamanaka <peter@popinnow.com>
2019-05-28 15:53:10 -0700

    Correct business plan

--- assets/js/instapage.snippet.js
+++ assets/js/instapage.snippet.js
@@ -9,7 +9,7 @@ var paramAdGclid = getUrlParameter('gclid') || 'none';
 var pp = {
   env: 'app',
   payment: {
-    monthly: ['free', 'premium_monthly', 'business', 'enterprise'],
+    monthly: ['free', 'premium_monthly', 'business_monthly', 'enterprise'],
     education: ['edu_free', 'edu_free', 'edu_free', 'edu_free'],
     conference: ['conf_free', 'conf_free', 'conf_free']
   },
